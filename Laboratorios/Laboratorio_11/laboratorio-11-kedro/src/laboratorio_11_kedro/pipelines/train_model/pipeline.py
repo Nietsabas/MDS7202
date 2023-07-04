88caf59b1b1c08859c01e27dd09d00071d124e2c
josuezito <josue.guillenambulay@gmail.com>
2023-07-04 03:00:32 -0400

    Parte 4.2 lista archivos modificados, funciones creadas

--- Laboratorios/Laboratorio_11/laboratorio-11-kedro/src/laboratorio_11_kedro/pipelines/train_model/pipeline.py
+++ Laboratorios/Laboratorio_11/laboratorio-11-kedro/src/laboratorio_11_kedro/pipelines/train_model/pipeline.py
@@ -4,7 +4,63 @@ generated using Kedro 0.18.10
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from nodes import split_data, train_model, evaluate_model
+from kedro.io import DataCatalog
+from kedro.runner import SequentialRunner
 
 
-def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            node(
+                split_data,
+                inputs="data",
+                outputs=[
+                    "X_train",
+                    "X_valid",
+                    "X_test",
+                    "y_train",
+                    "y_valid",
+                    "y_test",
+                ],
+                name="split_data_node",
+            ),
+            node(
+                train_model,
+                inputs=[
+                    "X_train",
+                    "X_valid",
+                    "y_train",
+                    "y_valid",
+                ],
+                outputs="best_model",
+                name="train_model_node",
+            ),
+            node(
+                evaluate_model,
+                inputs=["best_model", "X_test", "y_test"],
+                outputs=None,
+                name="evaluate_model_node",
+            ),
+        ]
+    )
+
+
+def run_pipelines(pipeline, catalog):
+    runner = SequentialRunner()
+    runner.run(pipeline, catalog)
+
+
+if __name__ == "__main__":
+    parameters = dict(
+        split_params=dict(
+            target="price",
+            train_ratio=0.8,
+            valid_ratio=0.1,
+            random_state=67
+        )
+    )
+    catalog = DataCatalog.from_config("conf/base/catalog.yml")
+    pipeline = create_pipeline(**parameters)
+    run_pipelines(pipeline, catalog)
+
